86fb6d7582c725b09b7a95f5a76c6ac23328c842
Liyuan Xu <liyuan9988@gmail.com>
2025-05-31 16:47:45 +0100

    formatting

--- docs/notebooks/log_expected_improvement.pct.py
+++ docs/notebooks/log_expected_improvement.pct.py
@@ -38,16 +38,19 @@ import matplotlib.pyplot as plt
 import gpflow
 from trieste.models.gpflow import GaussianProcessRegression, build_gpr
 
+
 ## Defining a problem
 def forrester_true(x):
-    return (6.*x - 2)**2 * tf.sin(12.*x-4)
+    return (6.0 * x - 2) ** 2 * tf.sin(12.0 * x - 4)
+
 
 def forrester_sim(x):
     y = forrester_true(x)
     noise = tf.random.normal(y.shape, 0.0, 0.25, dtype=y.dtype)
     return y + noise
 
-search_space = trieste.space.Box([0.0], [1.])
+
+search_space = trieste.space.Box([0.0], [1.0])
 f_observer = trieste.objectives.utils.mk_observer(forrester_sim)
 
 ## Build gpflow model
@@ -79,7 +82,11 @@ plt.legend()
 # We can compute the original EI and logEI on this GP model. Note that we use different axis for each aquisition function in the for the ease of comparison.
 
 # %%
-from trieste.acquisition.function import ExpectedImprovement, LogExpectedImprovement
+from trieste.acquisition.function import (
+    ExpectedImprovement,
+    LogExpectedImprovement,
+)
+
 acq_EI_func = ExpectedImprovement().prepare_acquisition_function(m, data)
 acq_logEI_func = LogExpectedImprovement().prepare_acquisition_function(m, data)
 
@@ -89,26 +96,28 @@ X_grid = np.linspace(0.0, 1.0, 100)
 log_EI_val = acq_logEI_func(X_grid[:, None, None])
 EI_val = acq_EI_func(X_grid[:, None, None])
 
+
 def plot_EI_and_logEI(X_grid, log_EI_val, EI_val):
     fig, ax1 = plt.subplots()
-    color = 'tab:red'
-    ax1.set_xlabel('x')
-    ax1.set_ylabel('logEI', color=color)
-    ax1.plot(X_grid, log_EI_val[:, 0], color=color, label = 'logEI')
-    ax1.tick_params(axis='y', labelcolor=color)
+    color = "tab:red"
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("logEI", color=color)
+    ax1.plot(X_grid, log_EI_val[:, 0], color=color, label="logEI")
+    ax1.tick_params(axis="y", labelcolor=color)
 
     ax2 = ax1.twinx()
 
-    color = 'tab:blue'
-    ax2.set_ylabel('EI', color=color)
-    ax2.plot(X_grid, EI_val[:, 0], color=color, label = 'EI')
-    ax2.tick_params(axis='y', labelcolor=color)
+    color = "tab:blue"
+    ax2.set_ylabel("EI", color=color)
+    ax2.plot(X_grid, EI_val[:, 0], color=color, label="EI")
+    ax2.tick_params(axis="y", labelcolor=color)
 
     lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
     lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
-    fig.legend(lines, labels, loc='lower right', bbox_to_anchor=(0.87, 0.15))
+    fig.legend(lines, labels, loc="lower right", bbox_to_anchor=(0.87, 0.15))
     fig.tight_layout()
-    
+
+
 plot_EI_and_logEI(X_grid, log_EI_val, EI_val)
 
 # %% [markdown]
@@ -119,29 +128,36 @@ plot_EI_and_logEI(X_grid, log_EI_val, EI_val)
 from trieste.experimental.plotting import plot_regret
 from trieste.acquisition.rule import EfficientGlobalOptimization
 
+
 ## Defining a SoS problem
 def SoS(x):
     return tf.reduce_sum((x - 0.5) ** 2, keepdims=True, axis=1)
 
 
-def benchmark_SoS(search_space, observer, acq_qule_builder, initial_data, n_step, ax):
-  
-    gpflow_model = trieste.models.gpflow.build_gpr(initial_data, search_space, likelihood_variance=1e-7)
-    model = trieste.models.gpflow.GaussianProcessRegression(gpflow_model, num_kernel_samples=100)
-    
-    bo = trieste.bayesian_optimizer.BayesianOptimizer(observer,search_space)
+def benchmark_SoS(
+    search_space, observer, acq_qule_builder, initial_data, n_step, ax
+):
+
+    gpflow_model = trieste.models.gpflow.build_gpr(
+        initial_data, search_space, likelihood_variance=1e-7
+    )
+    model = trieste.models.gpflow.GaussianProcessRegression(
+        gpflow_model, num_kernel_samples=100
+    )
+
+    bo = trieste.bayesian_optimizer.BayesianOptimizer(observer, search_space)
     acq_rule = EfficientGlobalOptimization(builder=acq_qule_builder)
     results = bo.optimize(
         n_step, initial_data, model, acquisition_rule=acq_rule
     )
-    
-    #plotting
+
+    # plotting
     dataset = results.try_get_final_dataset()
     query_points = dataset.query_points.numpy()
     observations = dataset.observations.numpy()
-    
+
     _, min_obs, arg_min_idx = results.try_get_optimal_point()
-    suboptimality = observations #the true optimal score is zero
+    suboptimality = observations  # the true optimal score is zero
     plot_regret(
         suboptimality, ax, num_init=num_initial_points, idx_best=arg_min_idx
     )
@@ -151,38 +167,37 @@ def benchmark_SoS(search_space, observer, acq_qule_builder, initial_data, n_step
     return min_obs[0]
 
 
-
 num_initial_points = 10
 n_step = 50
 ndim = 10
-search_space = trieste.space.Box([0.0]  * ndim, [1.]  * ndim)
+search_space = trieste.space.Box([0.0] * ndim, [1.0] * ndim)
 observer = trieste.objectives.utils.mk_observer(SoS)
 
 initial_query_points = search_space.sample_sobol(num_initial_points)
 initial_data = observer(initial_query_points)
-    
+
 fig, ax = plt.subplots(1, 2)
 EI_min_obs = benchmark_SoS(
-    search_space, 
+    search_space,
     observer,
     trieste.acquisition.function.ExpectedImprovement(),
     initial_data,
     n_step,
-    ax[0]
+    ax[0],
 )
 ax[0].set_title(f"Regret for EI \n (min = {EI_min_obs:.2e})")
 
 
 log_EI_min_obs = benchmark_SoS(
-    search_space, 
+    search_space,
     observer,
     trieste.acquisition.function.LogExpectedImprovement(),
     initial_data,
     n_step,
-    ax[1]
+    ax[1],
 )
 ax[1].set_title(f"Regret for LogEI \n (min = {log_EI_min_obs:.2e})")
-    
+
 fig.tight_layout()
 
 
